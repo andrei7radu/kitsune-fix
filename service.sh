ec426d260086b59ea6431cfb05e673969ae424d8
andrei7radu <74138663+andrei7radu@users.noreply.github.com>
2026-04-07 11:14:08 +0200

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,1 +1,24 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
 
+until [ "$(getprop sys.boot_completed)" = "1" ]; do
+    sleep 5
+done
+
+(
+    PKG="io.github.x0eg0.magisk"
+    cooldown=0
+
+    logcat -v brief -e "ActivityManager: Killing.*$PKG|Failed to stat.*io.github.huskydg.magisk" | while read -r line; do
+        
+        if [ "$cooldown" -eq 1 ]; then continue; fi
+        cooldown=1
+
+        am force-stop $PKG
+        sleep 1
+
+        am broadcast -a android.intent.action.BOOT_COMPLETED -p $PKG -f 32 > /dev/null 2>&1
+
+        ( sleep 5; cooldown=0 ) &
+    done 
+) &
